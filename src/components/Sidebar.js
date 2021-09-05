12ce764b148f808b203ac23c757e463c49e9ee51
Regoloale <72489833+Regoloale@users.noreply.github.com>
2021-09-05 17:59:55 +0200

    Trying to fix firebase issue

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,6 +13,9 @@ import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import AppsIcon from '@material-ui/icons/Apps';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
+import { Card } from '@material-ui/core';
 
 
 function Sidebar() {
@@ -37,6 +40,10 @@ function Sidebar() {
             <SidebarOption Icon={AppsIcon} title="Apps" />
             <SidebarOption Icon={FileCopyIcon} title="File browser" />
             <SidebarOption Icon={ExpandLessIcon} title="Show less" />
+            <hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
+            <hr />
+            <SidebarOption Icon={AddIcon} title="Add Channel" addChannelOption />
 
         </SidebarContainer>
     )
@@ -51,6 +58,12 @@ const SidebarContainer = styled.div`
     border-top: 1px solid #49274b;
     max-width: 260px;
     margin-top: 60px;
+
+    > hr {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #49274b;
+    }
 `;
 
 const SidebarHeader = styled.div`
